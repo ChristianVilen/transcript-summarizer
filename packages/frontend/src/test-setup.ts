cacd949a9fdbce9b33d7a3a3abcb206783581d83
Christian Vilen <christian.vilen@sok.fi>
2026-03-26 10:34:13 +0200

    fix: frontend unit test

--- packages/frontend/src/test-setup.ts
+++ packages/frontend/src/test-setup.ts
@@ -1,1 +1,14 @@
 Element.prototype.scrollIntoView = () => {};
+
+Object.defineProperty(window, "matchMedia", {
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+    addListener: () => {},
+    removeListener: () => {},
+  }),
+});
